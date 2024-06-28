0025331d3824799e6eb3df6ef93e0bd8e29bef4e
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-06-28 14:48:14 +0200

    [UPD] the script can now take the info writted by the user in the form as a value of "user_mail"

--- mail/script/script.js
+++ mail/script/script.js
@@ -13,3 +13,16 @@ stampa un messaggio appropriato sull’esito del controllo.
 3 - Avviare tramite ciclo un controllo che confronterà "user_mail" con ogni dato contenuto nell'array e, qualora il dato non dovesse trovare un valore uguale a se stesso, verrà stampato un messaggio che nega l'accesso. OPPURE uno di avvenuto accesso al sito
 */
 
+//--------------------------------------------------------------------
+
+// Richiamo nel codice il bottone che avvierà lo script:
+
+const button = document.getElementById('send');
+
+// Faccio in modo che lo script, al click del bottone raccolga i dati inseriti dall'utente
+
+button.addEventListener('click', function(){
+    let user_mail = document.getElementById("user_mail").value;
+
+    console.log(user_mail)
+})
