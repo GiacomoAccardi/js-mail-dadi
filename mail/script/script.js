2a9afe67f562cd77426cd1130ac07c972345e8f2
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-06-28 14:34:03 +0200

    [UPD] there's now instructions as a comment in the js file linked to "mail.html"

---
+++ mail/script/script.js
@@ -0,0 +1,15 @@
+/*
+Mail
+Chiedi all’utente la sua email (tramite prompt o input, a voi la scelta),
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+
+1 - Chiedere all'utente la mail tramite l'utilizzo di input.
+     ci sarà dunque la variabile "user_mail" che prenderà al, click di un pulsante, il valore inserito dall'utente.
+
+2 - Creare un array che contenga al suo interno tutte le mail degli utenti che possono accedere 
+     Ci sarà dunque la variabile "registered_users" che avrà come valore l'array
+
+3 - Avviare tramite ciclo un controllo che confronterà "user_mail" con ogni dato contenuto nell'array e, qualora il dato non dovesse trovare un valore uguale a se stesso, verrà stampato un messaggio che nega l'accesso. OPPURE uno di avvenuto accesso al sito
+*/
+
