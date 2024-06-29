45dfd49128f5b546dcfd50f1ee6de929f7d8755e
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-06-29 14:17:04 +0200

    [UPD] There's now some instruction for the "dices" exercize

---
+++ dadi/script/script.js
@@ -0,0 +1,13 @@
+/*
+
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+1 - creo un button con la quale far partire lo script
+2 - al click, verranno generati due numeri casuali da 1 a 6 "giocatore" e "avversario"
+3 - I due numeri verranno confrontati e il vincitore visualizzerà un messaggio di vittoria nel caso di "giocatore" > "avversario", di sconfitta nel caso "giocatore" < "avversario" e, infine di pareggio se "giocatore" = "avversario"
+
+----------------------------------------------------------------------------------------------------------
+*/
+
